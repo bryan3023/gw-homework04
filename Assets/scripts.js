5df5b87118c812653ee0a8e0e1eb8e189502b59c
Bryan <bryan.fennell@hotmail.com>
2020-04-08 16:57:31 -0400

    Tweaked HTML, cleaned up events, and implemented an end of quiz pane.

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,74 +1,138 @@
 /*
-  Implements the game along a simplistic model-view-controller pattern.
+  Implements the game along a simplistic model-view-controller
+  pattern. The program is organized along the following
+  guidelines:
+   - Neither the model nor the controller can access the DOM.
+   - Neither the model nor the view can directly interact with the controller.
+   - The view cannot directly interact with the model or vice versa.
+   - The controller should only interact with the model and view via
+     provided methods.
  */
-"use strict";
+
+ "use strict";
 
 
 let QuizModel = {
 
-  Questions: [
+  questions: [
     {
-      Id: 0,
-      Question: "Are null values and undefined values the same?",
-      Answers: [
-        {
-          Text: "Yes"
-        },
-        {
-          Text: "No",
-          Correct: true        }
+      id: 0,
+      text: "Are null values and undefined values the same?",
+      answers: [
+        { text: "Yes" },
+        { text: "No", correct: true }
+      ]
+    },
+    {
+      id: 1,
+      text: "test",
+      answers: [
+        { text: "a" },
+        { text: "b", correct: true }
       ]
     }
   ],
 
-  ScoreBoard: [],
+  scoreBoard: [],
 
-  EndGamePane: {
-    Title: "All done!",
-    Text: [
-      "Your score was {0}."
-    ]
+  gameParameters: {
+    countdownStartSeconds: 90,
+    incorrectPenaltySeconds: 15,
+    pointsPerCorrectAnswer: 5
   },
 
-  CurrentGame: {
-    Timer: {
-      CountdownStartSeconds: 90,
-      IncorrectPenaltySeconds: 15,
-      TimeRemaingSeconds: 90
-    },
-    Score: 0,
-    AskedQuestions: []
+  currentGame: {
+    timeRemaingSeconds: 90,
+    countCorrectAnswers: 0,
+    askedQuestions: []
   },
 
 
+  /*
+    If available, return a new question and append its ID to the list of those
+    that have been asked. If there are no more questions, return null.
+   */
   getNewQuestion:
     function() {
       let
-        available = this.getAvailableQuestions(),
-        selected = available[available.length - 1];
-    
-      QuizModel.CurrentGame.AskedQuestions.push(selected.Id);
-      return selected;
+        available = this.getAvailableQuestions();
+      
+      if (0 !== available.length) {
+        let selected = available[available.length - 1];
+        this.currentGame.askedQuestions.push(selected.id);
+        return selected;
+      } else {
+        return null;
+      }
     },
 
+
+  /*
+    From the list of available questions, return those that haven't
+    been asked yet.
+   */
   getAvailableQuestions:
     function() {
-      return this.Questions.filter(
-        q => -1 === this.CurrentGame.AskedQuestions.indexOf(q.Id));
+      return this.questions.filter(
+        q => -1 === this.currentGame.askedQuestions.indexOf(q.id));
     },
 
+
+  /*
+    Return true if the answer provided matches the correct answer
+    for the current question.
+   */
   isAnswerCorrect:
-    function(answerIndex) {
-      let current = this.Questions
-        .filter(q => q.Id == getCurrentQuestionId())[0];
-    
-      return current.Answers[answerIndex].Correct;
+    function(answer) {
+      let
+        currentQuestion = this.questions.filter(q => q.id === this.getCurrentQuestionId())[0],
+        correctAnswer = currentQuestion.answers.filter(a => a.correct)[0];
+
+      return correctAnswer.text === answer;
     },
 
+
+  /*
+    Return the current (i.e., last) question from the list of
+    those that have been asked.
+   */
   getCurrentQuestionId:
     function() {
-      let asked = this.CurrentGame.AskedQuestions;
+      let asked = this.currentGame.askedQuestions;
       return asked[asked.length - 1];
+    },
+
+  /*
+    Return the final score of the game.
+   */
+  getScore:
+    function() {
+      return this.currentGame.countCorrectAnswers * this.gameParameters.pointsPerCorrectAnswer;
+    },
+
+
+  /*
+   */
+  increaseScore:
+    function() {
+      this.currentGame.countCorrectAnswers++;
+    },
+
+  startTimer:
+    function(timerCallback) {
+
+    },
+
+  substractTimePenalty:
+    function() {
+      this.currentGame.timeRemaingSeconds -= this.gameParameters.incorrectPenaltySeconds;
+    },
+
+  reset:
+    function() {
+      this.currentGame.timeRemaingSeconds = this.gameParameters.countdownStartSeconds;
+      this.currentGame.countCorrectAnswers = 0;
+      this.currentGame.askedQuestions = [];
     }
 };
 
@@ -79,29 +143,66 @@ let QuizController = {
   model: null,
   view: null,
 
+
+  /*
+    Game entry point. Initialize the state and show the welcome pane, providing an
+    event to begin the quiz.
+   */
   start:
     function() {
       this.model = QuizModel;
       this.view = QuizView;
 
-      this.view.showQuizWelcomePane();
+      this.view.showQuizWelcomePane(() => {
+        event.preventDefault();
+        QuizController.beginQuiz();
+      });
     },
 
+
+  /*
+    Start the timer nd 
+   */
   beginQuiz:
     function() {
-      let question = this.model.getNewQuestion();
-      this.view.showQuestionPane(question);
+      this.model.reset();
+      this.model.startTimer(() => {
+        event.preventDefault();
+        QuizController.endQuiz();
+      });
+      this.getNextQuestion();
     },
 
   evaluateAnswer:
-    function(answerIndex) {
-      if (this.model.isAnswerCorrect(answerIndex)) {
+    function(answer) {
+      if (this.model.isAnswerCorrect(answer)) {
         this.view.setQuizStatus("Correct!");
+        this.model.increaseScore();
       } else {
         this.view.setQuizStatus("Wrong!");
+        this.model.substractTimePenalty();
       }
+      this.getNextQuestion();
     },
 
+
+  /*
+    Get a new question and create a new question pane for it, providing an
+    event listener to check answers. If there are no more questions, end the
+    game.
+   */
+  getNextQuestion:
+    function() {
+      let question = this.model.getNewQuestion();
+      if (question) {
+        this.view.showQuestionPane(question, answer => {
+            event.preventDefault();
+            QuizController.evaluateAnswer(answer);
+        });
+      } else {
+        this.endQuiz();
+      }
+    },
 
   saveScoreBoard:
     function() {
@@ -110,8 +211,8 @@ let QuizController = {
 
   loadScoreBoard:
     function() {
-      QuizModel.ScoreBoard = localStorage.getItem("ScoreBoard");
-      if (null === QuizModel.ScoreBoard) {
+      QuizModel.scoreBoard = localStorage.getItem("ScoreBoard");
+      if (null === QuizModel.scoreBoard) {
         QuizModel.ScoreBoard = [];
       }
     },
@@ -130,92 +231,122 @@ let QuizController = {
       }
       this.saveScoreBoard();
     },
-  
-  getQuestion:
-    function() {
-      let availableQuestions = QuizModel.Questions.filter(q => !q.Asked);
-
-        chosenQuestion = availableQuestions[getRandomInt(0, availableQuestions.length - 1)];
-
-      QuizModel.Questions.filter(q => q.Id === chosenQuestion.Id).Asked = true;
-      return chosenQuestion;
-    },
 
   endQuiz:
     function() {
-
-    },
-
-  resetQuiz:
-    function() {
-      QuizModel.CurrentGame.Score = 0;
+      this.view.showQuizDonePane(this.model.getScore());
     }
-
 };
 
 
 let QuizView = {
 
-  WelcomePane: {
-    Title: "Coding Quiz Challenge",
-    Text: [
+  welcomePane: {
+    title: "Coding Quiz Challenge",
+    text: [
       "Welcome to the JavaScript coding quiz!",
       "Test your knowledge of JavaScript by seeing how many you can get right. You will have 90 seconds to complete the quiz, but you'll lose 15 for every wrong answer.",
       "Good luck!"
     ]
   },
 
+  endGamePane: {
+    title: "All done!",
+    text: "Your score was {0}."
+  },
+
+
 
   showQuizWelcomePane:
-    function() {
-      this.getQuizHeader().textContent = this.WelcomePane.Title;
-    
+    function(beginCallback) {
+      this.getQuizHeader().textContent = this.welcomePane.title;    
       this.clearQuizContent();
     
-      for (let paragraph of this.WelcomePane.Text) {
+      for (let paragraph of this.welcomePane.text) {
         let pTag = document.createElement("p");
         pTag.textContent = paragraph;
         this.getQuizContent().appendChild(pTag)
       }
     
       this.getQuizContent().appendChild(
-        this.addQuizNavButton("Begin", "QuizController.beginQuiz()"));
+        this.addQuizNavButton("Begin", beginCallback)
+      );
     
       this.hideQuizStatus();
     },
 
   showQuestionPane:
-    function(question) {
-      this.getQuizHeader().textContent = question.Question;
-    
+    function(question, answerCallback) {
+      this.getQuizHeader().textContent = question.text;
       this.clearQuizContent();
     
-      for (let i =0 ;i < question.Answers.length; i++) {
-        let answerButton = this.addQuizOptionButton(question.Answers[i].Text, i);
+      for (let answer of question.answers) {
+        let answerButton = this.addQuizAnswerButton(answer.text, answerCallback);
         this.getQuizContent().appendChild(answerButton);
       }
     },
 
+  showQuizDonePane:
+    function(score) {
+      this.getQuizHeader().textContent = this.endGamePane.title;
+      this.clearQuizContent();
+
+      let pTag = document.createElement("p");
+      pTag.textContent = this.endGamePane.text.replace("{0}", score);
+      this.getQuizContent().appendChild(pTag);
+
+      this.getQuizContent().appendChild(this.addScoreBoardForm());
+    },
+
+
+  addScoreBoardForm:
+    function(formCallback) {
+      let formTag = document.createElement("form");
+      formTag.id = "quiz-scoreboard-form";
+      formTag.className = "form-inline";
+
+      let divTag = document.createElement("div")
+      divTag.className = "form-group w-100";
+
+      let labelTag = document.createElement("label");
+      labelTag.textContent = "Enter your initials: ";
+      labelTag.className = "col-form-label";
+      labelTag.htmlFor = "player-initials"
+
+      let inputTag = document.createElement("input");
+      inputTag.id = "player-initials";
+      inputTag.type = "text";
+      inputTag.className = "form-control mx-0 mx-md-3"
+
+      divTag.appendChild(labelTag);
+      divTag.appendChild(inputTag);
+      divTag.appendChild(this.addQuizNavButton("Submit", null))
+      formTag.appendChild(divTag);
+
+      return formTag;
+    },
+
   addQuizNavButton:
-    function(buttonText, onClickText) {
+    function(buttonText, buttonCallback) {
       let button = document.createElement("button");
     
-      button.setAttribute("type", "button");
       button.setAttribute("class", "btn btn-info text-left mr-1");
-      button.setAttribute("onclick", onClickText);
+      button.addEventListener("click", buttonCallback);
+      button.type = "submit";
       button.textContent = buttonText;
     
       return button;
     },
     
-  addQuizOptionButton:
-    function(text, answerIndex) {
+  addQuizAnswerButton:
+    function(buttonText, buttonCallback) {
       let button = document.createElement("button");
     
       button.setAttribute("type", "button");
       button.setAttribute("class", "btn btn-lg btn-block btn-info text-left");
-      button.setAttribute("onclick", `QuizController.evaluateAnswer(${answerIndex});`);
-      button.textContent = text;
+      button.addEventListener("click", () => buttonCallback(button.value));
+      button.textContent = buttonText;
+      button.value = buttonText;
     
       return button;
     },
@@ -256,8 +387,6 @@ let QuizView = {
     function() {
       return document.querySelector("#quiz-status");
     }
-    
-
 };
 
 /*
